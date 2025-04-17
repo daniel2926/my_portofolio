045a27864f2af9ddd6435ef814b12a1c0060b7fb
Daniel <danielsimamorasimamora@gmail.com>
2025-04-17 15:33:03 +0700

    terakhir code yang di buat

---
+++ lib/widgets/skill_badge.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SkillBadge extends StatelessWidget {
+  final String title;
+  final String logo;
+  const SkillBadge({super.key, required this.title, required this.logo});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
